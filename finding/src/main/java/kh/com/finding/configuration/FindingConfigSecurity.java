2bccc14000eeea8a0b4c6641848463e920f9b5e0
ekosal2014 <ekosal2014@gmail.com>
2018-04-13 17:49:37 +0900

    sdg

--- finding/src/main/java/kh/com/finding/configuration/FindingConfigSecurity.java
+++ finding/src/main/java/kh/com/finding/configuration/FindingConfigSecurity.java
@@ -1,5 +1,29 @@
 package kh.com.finding.configuration;
 
-public class FindingConfigSecurity {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+public class FindingConfigSecurity extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private CustomAuthenticationProvider  customAuthenticationProvider;
+ 
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		auth.authenticationProvider(this.customAuthenticationProvider);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(http);
+	}
+
+	
+	
 }
