9a19581f121d4e84565c31a7e9afe1b9106ab161
ekosal2014 <ekosal2014@gmail.com>
2018-05-17 14:01:09 +0900

    dsfasd

--- finding/src/main/java/kh/com/finding/utils/UploadFileUtils.java
+++ finding/src/main/java/kh/com/finding/utils/UploadFileUtils.java
@@ -3,6 +3,8 @@ package kh.com.finding.utils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.springframework.util.FileCopyUtils;
@@ -13,28 +15,39 @@ import kh.com.finding.entities.Images;
 public class UploadFileUtils {
 	/**
 	 * 
-	 * @param image
-	 * @param file
-	 * @param path
-	 * @return
+	 * @param image : name of image that uploaded.
+	 * @param file  : image that is going to upload.
+	 * @param path  : path that upload image this path.
+	 * @return 
 	 */
 	public static boolean UploadSingleFile(Images image, MultipartFile file,String path){
 		
 		if (!file.isEmpty()){
-			
-			
+
 			try {
 				//System.out.println(DateUtils.getDateTime(DateUtils.dttms_format));
-			
-				String destination = path + File.separator + file.getOriginalFilename();
-				String file_name   = DateUtils.getDateTime(DateUtils.dttms_format)+".jpg";
+				// * --------------------------------------------------------------------
+				// * initialize parameter for this method 
+				// *---------------------------------------------------------------------
+			    String exstention  = file.getOriginalFilename().substring(file.getOriginalFilename().indexOf("."),file.getOriginalFilename().length());			    
+				String destination = path + File.separator + file.getOriginalFilename();         // path that to upload image.
+				String file_name   = DateUtils.getDateTime(DateUtils.dttms_format)+ exstention ; // file name that change already
 				
+				// * --------------------------------------------------------------------
+				// * create new file image 
+				// *---------------------------------------------------------------------
 				File f = new File(destination);
 				File nf= new File(path + File.separator + file_name);
-
+				
+				// * --------------------------------------------------------------------
+				// * copy file to new file and than move file to another file.
+				// *---------------------------------------------------------------------
 				FileCopyUtils.copy(file.getBytes(), f);				
 				FileUtils.moveFile(f, nf);
 				
+				// * --------------------------------------------------------------------
+				// * set file name to insert in table
+				// *---------------------------------------------------------------------
 				image.setImage(file_name);
 				
 				return true;
@@ -47,12 +60,72 @@ public class UploadFileUtils {
 			
 			
 		}else{
+			// * --------------------------------------------------------------------
+			// * if you didn't upload image 
+			// * system will be upload default image
+			// *---------------------------------------------------------------------
+			image.setImage("default_logo.png");  
 			return true;
 		}
 		
 	}
-	
-	public static void UploadMultiFile(Images image, MultipartFile[] file, String path){
+	/**
+	 * method : upload multiple image  
+	 * @param image
+	 * @param files
+	 * @param path
+	 * @return
+	 */
+	public static boolean UploadMultiFile(Images image, MultipartFile[] files, String path){
+		
+		List<String> lst_f_name = new ArrayList<>();
+		
+		try{
+			
+			for ( MultipartFile file : files ){
+				// * --------------------------------------------------------------------
+				// * if this file didn't have image than different image will be upload.
+				// *---------------------------------------------------------------------
+				if ( file.isEmpty() ){
+					continue;
+				}
+				
+				// * --------------------------------------------------------------------
+				// * initialize parameter for this method 
+				// *---------------------------------------------------------------------
+				String exstention  = file.getOriginalFilename().substring(file.getOriginalFilename().indexOf("."),file.getOriginalFilename().length());						
+				String destination = path + File.separator + file.getOriginalFilename();
+				String file_name   = DateUtils.getDateTime(DateUtils.dttms_format)+ exstention ;
+				// * --------------------------------------------------------------------
+				// * create new file image 
+				// *---------------------------------------------------------------------
+				File f = new File(destination);
+				File nf= new File(path + File.separator + file_name);
+				// * --------------------------------------------------------------------
+				// * copy file to new file and than move file to another file.
+				// *---------------------------------------------------------------------
+				FileCopyUtils.copy(file.getBytes(), f);				
+				FileUtils.moveFile(f, nf);
+				// * --------------------------------------------------------------------
+				// * set temporary file name list
+				// *---------------------------------------------------------------------
+				lst_f_name.add(file_name);
+				
+			}
+			// * --------------------------------------------------------------------
+			// * set file name to insert in table
+			// *---------------------------------------------------------------------
+			image.setListImage(lst_f_name);
+			
+			return true;
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+			
+		}
+		
+		
 		
 	}
 	
